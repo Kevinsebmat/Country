81b8566af1b1e0e888d24c415e0bd26d81e63472
Kevin Mathews <65047030+Kevinsebmat@users.noreply.github.com>
2023-02-19 02:04:52 -0800

    created countries page and mapped graphql data

---
+++ pages/countries/[code].js
@@ -0,0 +1,65 @@
+
+import {getAllCountries} from "@/lib/CountriesService";
+import Link from "next/link";
+import MyButton from "@/components/MyButton"
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+} from "@material-tailwind/react";
+import Image from "next/image";
+
+
+
+export async function getServerSideProps (context) {
+    const countries = await getAllCountries()
+    const country = countries.find(item => item.code === context.params.code)
+    return {props:{data:country}}
+}
+ 
+const Country = ({data}) => {
+    return (
+       <div class="grid h-screen place-items-center">
+
+       <Card key={data.name} className="w-96 bg-gray-100 shadow-2xl">
+            <CardHeader color="blue" className="relative h-56">
+            <Image
+                src={`https://countryflagsapi.com/png/${data.code}`}
+                alt="img-blur-shadow"
+                width={800}
+                height={500}
+              />{" "}
+            </CardHeader>{" "}
+            <CardBody className="text-center">
+              <Typography variant="h5" className="mb-2">
+                {" "}
+              </Typography>{" "}
+              <Typography> {data.name} {data.emoji}{" "} </Typography>{" "}
+            </CardBody>{" "}
+            <CardFooter
+              divider
+              className="flex items-center justify-between py-3"
+            >
+              <Typography variant="h1" color="blue" textGradient>
+                {" "}
+                Languages:{" "}
+              </Typography>{" "}
+              <Typography variant="small">
+                {" "}
+                {data.languages.map((language) => {
+                  return <div key={language.code}> {language.name} </div>;
+                })}{" "}
+              </Typography>{" "}
+              {/* <Typography variant="small" color="gray" className="flex gap-1">
+                                                                                                <i className="fas fa-map-marker-alt fa-sm mt-[3px]" />
+                                                                                                Barcelona, Spain{" "}
+                                                                                              </Typography>{" "} */}{" "}
+            </CardFooter>{" "}
+          </Card>
+          </div>
+    )
+}
+
+export default Country
---
+++ pages/countries/index.js
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import {getAllCountries} from "@/lib/CountriesService";
+import MyButton from "@/components/MyButton"
+import { Button } from "@material-tailwind/react";
+
+export async function getServerSideProps () {
+    const countries = await getAllCountries()
+    return {props:{data:countries}}
+}
+
+const Countries = ({data}) => {
+
+    return (
+        <div className={'px-4 py-5 flex flex-col bg-gray-400'}>
+        <div>
+        <MyButton href="/" appendClass="animate-bounceLight">Home</MyButton>
+        </div>
+        <div> </div>
+            <table>
+                <thead class="bg-white border-b sticky top-0 text-left ">
+                <tr>
+                    <th>Country (Click for Details)</th>
+                </tr>
+                </thead>
+                <tbody>
+                {data.map((country) => (
+                    <tr class="even:bg-amber-9 odd:bg-blue-100" key={country.code}>
+                        <td>
+                            <Link href={`/countries/${encodeURIComponent(country.code)}`} class="px-2 py-0 text-blue-100 no-underline bg-blue-500 rounded hover:bg-blue-600 hover:underline hover:text-blue-200">
+
+                            {country.name}
+                        </Link>
+                        </td>
+                        
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+
+        </div>
+    )
+}
+
+export default Countries
+
+
+
